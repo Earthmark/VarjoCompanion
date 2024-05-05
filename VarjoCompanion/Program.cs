c0f5eb6fc35180bec85a2e6bafc5c2b6e9a55f3c
Earthmark <earthmark.miller@gmail.com>
2024-05-04 23:52:21 -0700

    Updated to dotnet 8, in prep for logging eye tracking data for diagnostics.

---
+++ VarjoCompanion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using VarjoCompanion;
+
+IntPtr session = VarjoEyeTracking.Init();
+
+if (!VarjoEyeTracking.IsGazeAllowed())
+{
+    Console.WriteLine("Gaze tracking is not allowed! Please enable it in the Varjo Base!");
+    return;
+}
+
+VarjoEyeTracking.GazeInit();
+VarjoEyeTracking.SyncProperties();
+
+VarjoEyeTracking.GazeData gazeData = new VarjoEyeTracking.GazeData();
+
+using (var memMapFile = MemoryMappedFile.CreateNew("VarjoEyeTracking", Marshal.SizeOf(gazeData)))
+{
+    using var accessor = memMapFile.CreateViewAccessor();
+
+    Console.WriteLine("Eye tracking session has started!");
+    while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter))
+    {
+        gazeData = VarjoEyeTracking.GetGaze();
+        accessor.Write(0, ref gazeData);
+    }
+}
+
+VarjoEyeTracking.varjo_SessionShutDown(session);
